c5dee08655ba05643aeeffcf2abe2eb0288112a2
Ayla Crosley <swiedaa1@nku.edu>
2014-04-04 22:23:34 +0000

    Basic list functionality working. List display now working.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,27 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
   def new
     @lists = List.new
   end
   def create
-    @lists = List.new
+    @lists = List.new(list_params)
     if @lists.save
       redirect_to root_path, alert: "Successfully Created"
     else
       render 'new'
     end
+  end
+  def destroy
+    @lists = List.find(params[:id])
+    @lists.destroy
+
+    redirect_to root_path
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
