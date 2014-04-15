1efb771b2598d3819346b2229b689bbcc2394dba
Ayla Crosley <swiedaa1@nku.edu>
2014-04-15 01:46:57 +0000

    Add bootstrap and some formatting and styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ NkuProject::Application.routes.draw do
 
   resources :lists
   resources :users
+  resources :sessions
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    get 'logout' => :destroy
+  end
   root 'lists#index'
 end
