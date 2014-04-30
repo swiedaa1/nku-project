b852ff3c9d0da896359ace4e14bf839cf715aa5c
Ayla Crosley <swiedaa1@nku.edu>
2014-04-30 23:33:06 +0000

    Fix some error messages. Validate user email uniqueness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true,                                                                                                                                                                            
                     length: { minimum: 2 } 
+  validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
 end
